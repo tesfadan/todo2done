168a84e5f7824e9a3d86a435582c8d60271dac32
Tesfa Demissie <tesfadaniel1@gmail.com>
2021-08-04 08:37:43 -0400

    Created Task Component

---
+++ src/Components/Task.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let task = {
+        task: String,
+        id: Number,
+        complete: Boolean,
+        archived: Boolean
+    };
+</script>
+
+<div class="task">
+    <div class="taskContent">{task.content}</div>
+    <div class="taskOptions">
+        <button>Mark Done</button>
+        <button>Edit</button>
+        <button>Delete</button>
+    </div>
+</div>
+
+<style>
+
+</style>
