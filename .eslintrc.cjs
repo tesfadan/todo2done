3ab23deb7e906af7874090aa9a7af1408cf6bbf8
Tesfa Demissie <tesfadaniel1@gmail.com>
2021-08-05 17:21:50 -0400

    Added netlify adapter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,20 @@
+const netlify = require('@sveltejs/adapter-netlify')
+
 module.exports = {
-	root: true,
-	extends: ['eslint:recommended', 'prettier'],
-	plugins: ['svelte3'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2019
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
+    root: true,
+    extends: ['eslint:recommended', 'prettier'],
+    plugins: ['svelte3'],
+    overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2019
+    },
+    env: {
+        browser: true,
+        es2017: true,
+        node: true
+    },
+    kit: {
+        adapter: netlify()
+    }
 };
